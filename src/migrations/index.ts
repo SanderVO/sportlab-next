f36f9b81f6c71446f0d3893d3da8e19f700c52d6
Sander <sander@Sanders-MacBook-Air.local>
2026-01-05 20:12:17 +0100

    Add generated types to repo

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251213_220954 from './20251213_220954';
 import * as migration_20251228_145802 from './20251228_145802';
+import * as migration_20260105_190801 from './20260105_190801';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251228_145802.up,
     down: migration_20251228_145802.down,
-    name: '20251228_145802'
+    name: '20251228_145802',
+  },
+  {
+    up: migration_20260105_190801.up,
+    down: migration_20260105_190801.down,
+    name: '20260105_190801'
   },
 ];
