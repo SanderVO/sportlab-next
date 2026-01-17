6d8769d093d4225e65f945005200fd094c12ef93
Sander <sander@Sanders-MacBook-Air.local>
2026-01-18 00:07:12 +0100

    Missing types

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 
 export default defineCloudflareConfig({
-    incrementalCache: withRegionalCache(r2IncrementalCache, {
-        mode: "long-lived",
-        bypassTagCacheOnCacheHit: true,
-    }),
+    incrementalCache: r2IncrementalCache,
     enableCacheInterception: true,
 });
