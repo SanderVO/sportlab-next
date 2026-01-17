37f56711df2be3c4c121d51b89a105b0ea2a2109
Sander <sander@Sanders-MacBook-Air.local>
2026-01-18 00:20:04 +0100

    Disable R2 cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-    incrementalCache: r2IncrementalCache,
-    enableCacheInterception: true,
-});
+// export default defineCloudflareConfig({
+//     incrementalCache: r2IncrementalCache,
+//     enableCacheInterception: true,
+// });
+
+export default defineCloudflareConfig();
