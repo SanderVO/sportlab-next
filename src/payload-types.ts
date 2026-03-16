24f4b24d19c070163e588d06058a43e6073d3746
Sander <sander@Sanders-MacBook-Air.local>
2026-03-16 18:15:51 +0100

    Fix types and payloadcms version

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -114,10 +114,9 @@ export interface Config {
     organization: OrganizationSelect<false> | OrganizationSelect<true>;
   };
   locale: null;
-  widgets: {
-    collections: CollectionsWidget;
+  user: User & {
+    collection: 'users';
   };
-  user: User;
   jobs: {
     tasks: {
       schedulePublish: TaskSchedulePublish;
@@ -212,7 +211,6 @@ export interface User {
       }[]
     | null;
   password?: string | null;
-  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -1713,16 +1711,6 @@ export interface OrganizationSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "collections_widget".
- */
-export interface CollectionsWidget {
-  data?: {
-    [k: string]: unknown;
-  };
-  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
