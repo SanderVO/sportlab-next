1c3cd542ccb502dfa233bfbb4bc79875cf3ec42f
Sander <sander@Sanders-MacBook-Air.local>
2026-03-30 21:30:14 +0200

    Robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next/types";
 
+export const dynamic = "force-dynamic";
+
 export default function robots(): MetadataRoute.Robots {
     const siteUrl =
         process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
