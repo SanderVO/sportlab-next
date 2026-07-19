36365e4cb866c965b609f3e29baf5821296cadee
Sander <sander@Sanders-MacBook-Air.local>
2026-07-19 21:05:16 +0200

    Enable local doQueue

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,8 +5,12 @@ import {
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import memoryQueue from "@opennextjs/cloudflare/overrides/queue/memory-queue";
 import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+
+const queue =
+    process.env.NODE_ENV === "development" ? memoryQueue : queueCache(doQueue);
 
 export default {
     ...defineCloudflareConfig({
@@ -15,7 +19,7 @@ export default {
         }),
         tagCache: d1NextTagCache,
         enableCacheInterception: true,
-        queue: queueCache(doQueue),
+        queue,
     }),
 } satisfies OpenNextConfig;
 
