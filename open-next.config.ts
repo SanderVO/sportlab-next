1ddaeb19efbe459fab8465afc81cfde8866a6393
Sander <sander@Sanders-MacBook-Air.local>
2026-07-19 20:59:16 +0200

    Add queue worker

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,6 +4,8 @@ import {
 } from "@opennextjs/cloudflare/config";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import queueCache from "@opennextjs/cloudflare/overrides/queue/queue-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 
 export default {
@@ -13,6 +15,7 @@ export default {
         }),
         tagCache: d1NextTagCache,
         enableCacheInterception: true,
+        queue: queueCache(doQueue),
     }),
 } satisfies OpenNextConfig;
 
